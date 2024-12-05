89ec9584f140ba4a500ac1aeabaa0c279d0a2ea8
Marijn Doeve <marijn@doeve.me>
2024-12-05 19:57:00 +0100

    Refactor Day 5

--- day5.py
+++ day5.py
@@ -1,59 +1,40 @@
 from collections import defaultdict
-from fileinput import FileInput
+from fileinput import FileInput, input
 from typing import Self
 
 
+class UpdateNumber:
+    page_rules: defaultdict[int, set[int]] = defaultdict(set)
+
+    def __init__(self, number: int) -> None:
+        self.number = number
+
+    def __lt__(self, other: Self) -> bool:
+        return other.number in UpdateNumber.page_rules[self.number]
+
+
 class Day5:
-    class UpdateNumber:
-        def __init__(self, number: int, rules: set[int]) -> None:
-            self.number = number
-            self.rules = rules
-
-        def __lt__(self, other: Self) -> bool:
-            return other.number in self.rules
-
     def __init__(self, reader: FileInput[str]) -> None:
-        self.page_rules: defaultdict[int, set[int]] = defaultdict(set)
-        self.total2 = 0
-
         for line in reader:
             if line == "\n":
                 break
             x, y = (int(x) for x in line.strip().split("|"))
-            self.page_rules[x].add(y)
+            UpdateNumber.page_rules[x].add(y)
 
-        updates = []
+        updates = [
+            [UpdateNumber(int(x)) for x in line.strip().split(",")] for line in reader
+        ]
 
-        for line in reader:
-            updates.append([int(x) for x in line.strip().split(",")])
-
-        total1 = 0
-        total2 = 0
+        total1 = total2 = 0
         for update in updates:
-            result = self.check_list(update)
-            if result > 0:
-                total1 += result
+            if (update_sorted := sorted(update)) == update:
+                total1 += update[len(update) // 2].number
                 continue
-
-            total2 += sorted(
-                [self.UpdateNumber(x, self.page_rules[x]) for x in update]
-            )[len(update) // 2].number
+            total2 += update_sorted[len(update) // 2].number
 
         print(f"Part 1: {total1}")
         print(f"Part 2: {total2}")
 
-    def check_list(self, update: list[int]) -> int:
-        for i, number in enumerate(update):
-            if number not in self.page_rules:
-                continue
-
-            if self.page_rules[number].intersection(update[:i]):
-                return 0
-
-        return update[len(update) // 2]
-
 
 if __name__ == "__main__":
-    import fileinput
-
-    Day5(fileinput.input())
+    Day5(input())
